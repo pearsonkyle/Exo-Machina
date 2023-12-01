e54744ba5dd85a7f1ef0ad2427713123b9aaea34
Kyle Pearson <kyle.a.pearson@jpl.nasa.gov>
2023-12-01 13:38:15 -0800

    encoder tests

--- doc2vec.py
+++ doc2vec.py
@@ -23,8 +23,6 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
 from sklearn.manifold import TSNE
 
-# torch device: cuda, mps, cpu
-device = "mps"
 
 # use tokenizer from spacy trained on sci corpus
 parser = spacy.load("en_core_web_trf",disable=["ner"])
@@ -288,21 +286,32 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Text Encoding Arguments')
     parser.add_argument('--chunking', type=int, default=100,
                         help='Chunk document by number of words')
-    parser.add_argument('--text_encoding', type=str, default='sentence-transformers/all-MiniLM-L6-v2',
+    parser.add_argument('--encoder', type=str, default='sentence-transformers/all-MiniLM-L6-v2',
                         help='Name of model from hugging face')
     parser.add_argument('--data_dir', type=str, default='markdown',
                         help='Directory of markdown files')
+    parser.add_argument('--output_dir', type=str, default='results',
+                        help='Directory to save results to')
     parser.add_argument('--spacy_tokenizer', action='store_true',
                         help='Use spacy tokenizer') # lemmanizer
+    parser.add_argument('--device', type=str, default='mps',
+                        help='Device to use for encoding (cuda, mps, cpu)')
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
 
     args = parse_args()
+
+    # create a directory to save results to
+    basename = os.path.basename(args.encoder)
+    dir_name = f"{basename}_{args.chunking}_{args.spacy_tokenizer}"
+    dir_name = os.path.join(args.output_dir, dir_name)
+    os.makedirs(dir_name, exist_ok=True)
     
     # get all markdown files
     markdown_files = glob.glob(os.path.join(args.data_dir, '*.md'))
+    markdown_files.sort()
 
     # separate file names into category labels
     # example: cat1-01.md cat1-02.md cat2-01.md cat2-02.md cat3-01.md cat4-01.md
@@ -319,35 +328,121 @@ if __name__ == '__main__':
 
     # convert to text
     processed_text = []
-    for i in range(len(markdown_files)):
+    for i in tqdm(range(len(markdown_files))):
         with open(markdown_files[i], 'r') as f:
-            processed_text.append(markdown_to_text(f.read()))
+            # convert markdown to text
+            text = markdown_to_text(f.read())
+
+            # lemmatize and remove stopwords
+            if args.spacy_tokenizer:
+                processed_text.append(spacy_tokenizer(text))
+            else:
+                processed_text.append(text)
 
     # chunk each document into ~100 word, 200 word, chunks
-    chunker = Chonker(chunking=200)
+    chunker = Chonker(chunking=args.chunking)
     chunked_text = []
-    chunked_labels = []
+    chunked_labels = []   # num
+    chunked_category = [] # str
     for i in range(len(processed_text)):
         # break text into chunks
         chunks = chunker(processed_text[i])
         chunked_text.extend(chunks)
         # repeat labels for each chunk
         chunked_labels.extend([labels[i]] * len(chunks))
+        chunked_category.extend([category[i]] * len(chunks))
+
+    # convert to numpy arrays
+    chunked_labels = np.array(chunked_labels)
+    chunked_category = np.array(chunked_category)
 
     # encode text
-    encoder = HuggingFaceEncoder(name='sentence-transformers/all-MiniLM-L6-v2', device='mps')
+    encoder = HuggingFaceEncoder(name=args.encoder, device=args.device)
     embeddings = encoder(chunked_text)
+
+    # create image plot of embeddings
+    fig, ax = plt.subplots(figsize=(10, 10))
+    im = ax.imshow(embeddings, cmap='jet', aspect='auto')
+    cbar = fig.colorbar(im, ax=ax)
+    cbar.set_label('Embedding Value')
+    ax.set_title('Embeddings')
+    plt.tight_layout()
+    plt.savefig(os.path.join(dir_name, 'embeddings.png'))
+    plt.close()
 
     # normalize embeddings across features with shape (n_samples, n_features)
     #n_embeddings = embeddings - np.mean(embeddings, axis=0)
     #n_embeddings = n_embeddings / np.std(n_embeddings, axis=0) # may lead to nans?
 
+    # create matrix of cosine similarities between chunks
+    cosine_sim = np.dot(embeddings, embeddings.T) / (np.linalg.norm(embeddings, axis=1) * np.linalg.norm(embeddings, axis=1)[:, None])
+
+    # plot cosine similarities
+    fig, ax = plt.subplots(figsize=(10, 10))
+    im = ax.imshow(cosine_sim, cmap='jet', vmin=-1, vmax=1)
+    cbar = fig.colorbar(im, ax=ax)
+    cbar.set_label('Cosine Similarity')
+    # change labels to category names on axes
+    ax.set_xticks(np.arange(len(chunked_category)))
+    ax.set_yticks(np.arange(len(chunked_category)))
+    ax.set_xticklabels(chunked_category, rotation=45)
+    ax.set_yticklabels(chunked_category, rotation=45)
+    ax.set_title('Cosine Similarity Between Chunks')
+    plt.tight_layout()
+    plt.savefig(os.path.join(dir_name, 'cosine_sim.png'))
+    plt.close()
+
+    # estimate in cluster similarity
+    avg_cos_in = []
+    for i in range(len(category_labels)):
+        avg_cos_in.append(np.mean(cosine_sim[chunked_labels == i][:, chunked_labels == i]))
+
+    # estimate out of cluster similarity
+    avg_cos_out = []
+    for i in range(len(category_labels)):
+        avg_cos_out.append(np.mean(cosine_sim[chunked_labels == i][:, chunked_labels != i]))
+
+    # plot in vs out of cluster similarity
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.scatter(avg_cos_in, avg_cos_out)
+    ax.set_xlabel('In Category Similarity')
+    ax.set_ylabel('Out of Category Similarity')
+    ax.set_title('In vs Out of Category Similarity')
+    for i, txt in enumerate(category_labels):
+        ax.annotate(txt, (avg_cos_in[i], avg_cos_out[i]))
+    plt.tight_layout()
+    plt.savefig(os.path.join(dir_name, 'in_vs_out.png'))
+    plt.close()
+
+    # compute average cosine similarity between categories
+    avg_cos = np.zeros((len(category_labels), len(category_labels)))
+    for i in range(len(category_labels)):
+        for j in range(len(category_labels)):
+            avg_cos[i, j] = np.mean(cosine_sim[chunked_labels == i][:, chunked_labels == j])
+
+    # plot average cosine similarity between categories
+    fig, ax = plt.subplots(figsize=(10, 10))
+    im = ax.imshow(avg_cos, cmap='jet', vmin=-1, vmax=1)
+    cbar = fig.colorbar(im, ax=ax)
+    cbar.set_label('Cosine Similarity')
+    # change labels to category names on axes
+    ax.set_xticks(np.arange(len(category_labels)))
+    ax.set_yticks(np.arange(len(category_labels)))
+    ax.set_xticklabels(category_labels, rotation=45)
+    ax.set_yticklabels(category_labels, rotation=45)
+    ax.set_title('Average Cosine Similarity Between Categories')
+    plt.tight_layout()
+    plt.savefig(os.path.join(dir_name, 'avg_cos.png'))
+    plt.close()
+
     # compute clustering metrics on chunked text
     scores = {
-        'silhouette': silhouette_score(embeddings, chunked_labels), # higher is better
-        'davies_bouldin': davies_bouldin_score(embeddings, chunked_labels), # lower is better
-        'calinski_harabasz': calinski_harabasz_score(embeddings, chunked_labels) # higher is better
+        'silhouette': float(silhouette_score(embeddings, chunked_labels)), # higher is better
+        'davies_bouldin': float(davies_bouldin_score(embeddings, chunked_labels)), # lower is better
+        'calinski_harabasz': float(calinski_harabasz_score(embeddings, chunked_labels)) # higher is better
     }
+
+    json.dump(scores, open(os.path.join(dir_name, 'scores.json'), 'w'), indent=4)
 
     print(scores)
 
@@ -364,32 +459,54 @@ if __name__ == '__main__':
 
     # plot data
     fig, axs = plt.subplots(2, 2, figsize=(10, 10))
-    im = axs[0,0].scatter(pca_text[:, 0], pca_text[:, 1], c=chunked_labels, cmap='tab20')
+    im = axs[0,0].scatter(pca_text[:, 0], pca_text[:, 1], c=chunked_labels, cmap='tab20', s=4)
     cbar = fig.colorbar(im, ax=axs[0,0])
     # change labels to category names
     cbar.set_ticks(np.arange(len(category_labels)))
     cbar.set_ticklabels(category_labels)
     axs[0,0].set_title('PCA')
     # plot lda
-    im = axs[1,0].scatter(lda_text[:, 0], lda_text[:, 1], c=chunked_labels, cmap='tab20')
+    im = axs[1,0].scatter(lda_text[:, 0], lda_text[:, 1], c=chunked_labels, cmap='tab20', s=4)
     cbar = fig.colorbar(im, ax=axs[1,0])
     # change labels to category names
     cbar.set_ticks(np.arange(len(category_labels)))
     cbar.set_ticklabels(category_labels)
     axs[1,0].set_title('LDA')
     # plot tsne
-    im = axs[0,1].scatter(tsne_text[:, 0], tsne_text[:, 1], c=chunked_labels, cmap='tab20')
+    im = axs[0,1].scatter(tsne_text[:, 0], tsne_text[:, 1], c=chunked_labels, cmap='tab20', s=4)
     cbar = fig.colorbar(im, ax=axs[0,1])
     # change labels to category names
     cbar.set_ticks(np.arange(len(category_labels)))
     cbar.set_ticklabels(category_labels)
     axs[0,1].set_title('t-SNE')
     # plot umap
-    im = axs[1,1].scatter(umap_text[:, 0], umap_text[:, 1], c=chunked_labels, cmap='tab20')
+    im = axs[1,1].scatter(umap_text[:, 0], umap_text[:, 1], c=chunked_labels, cmap='tab20', s=4)
     cbar = fig.colorbar(im, ax=axs[1,1])
     # change labels to category names
     cbar.set_ticks(np.arange(len(category_labels)))
     cbar.set_ticklabels(category_labels)
     axs[1,1].set_title('UMAP')
     plt.tight_layout()
-    plt.show()
+    plt.savefig(os.path.join(dir_name, 'embeddings.png'))
+    plt.close()
+
+    """
+    batch script for these models:
+
+    python doc2vec.py --chunking 100 --encoder sentence-transformers/all-MiniLM-L6-v2 --spacy_tokenizer
+    python doc2vec.py --chunking 100 --encoder sentence-transformers/all-MiniLM-L6-v2
+    python doc2vec.py --chunking 200 --encoder sentence-transformers/all-MiniLM-L6-v2 --spacy_tokenizer
+    python doc2vec.py --chunking 200 --encoder sentence-transformers/all-MiniLM-L6-v2
+    python doc2vec.py --chunking 100 --encoder sentence-transformers/all-mpnet-base-v2 --spacy_tokenizer
+    python doc2vec.py --chunking 100 --encoder sentence-transformers/all-mpnet-base-v2
+    python doc2vec.py --chunking 200 --encoder sentence-transformers/all-mpnet-base-v2 --spacy_tokenizer
+    python doc2vec.py --chunking 200 --encoder sentence-transformers/all-mpnet-base-v2
+    python doc2vec.py --chunking 100 --encoder sentence-transformers/multi-qa-MiniLM-L6-cos-v1 --spacy_tokenizer
+    python doc2vec.py --chunking 100 --encoder sentence-transformers/multi-qa-MiniLM-L6-cos-v1
+    python doc2vec.py --chunking 200 --encoder sentence-transformers/multi-qa-MiniLM-L6-cos-v1 --spacy_tokenizer
+    python doc2vec.py --chunking 200 --encoder sentence-transformers/multi-qa-MiniLM-L6-cos-v1
+    python doc2vec.py --chunking 100 --encoder facebook/dragon-plus-context-encoder --spacy_tokenizer
+    python doc2vec.py --chunking 100 --encoder facebook/dragon-plus-context-encoder
+    python doc2vec.py --chunking 200 --encoder facebook/dragon-plus-context-encoder --spacy_tokenizer
+    python doc2vec.py --chunking 200 --encoder facebook/dragon-plus-context-encoder    
+    """
